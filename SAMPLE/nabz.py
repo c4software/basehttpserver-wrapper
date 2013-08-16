eadf681526905f11a40864183ec063c04b12df5b
Valentin Brosseau <c4software@gmail.com>
2013-08-16 22:32:31 +0200

    Ajout d'un exemple
    Ajout d'un exemple tout simple.

--- SAMPLE/nabz.py
+++ SAMPLE/nabz.py
@@ -1,11 +1,42 @@
 import sys
+import argparse
+import subprocess
+import urllib
 sys.path.append('../') # Just to make the GIT source more cleaner without creating a module. In YOUR project you DON'T have to do that
 from extended_BaseHTTPServer import serve,route
 
-@route("/",["GET"])
-def main(**kwargs):
-	return "TODO"
+current_playing = []
+
+@route("/play",["GET"])
+def play(file=""):
+	if current_playing:
+		current_playing.pop().kill()
+
+	try:
+		current_playing.append(subprocess.Popen("{0} '{1}' vlc://quit".format(args.player, file[0]), shell=True))
+		return "{'stream':true}"
+	except:
+		return "{'stream':false}"
+
+@route("/stop",["GET"])
+def stop():
+	if current_playing:
+		current_playing.pop().kill()
+	return "{'stop':true}"
+
+@route("/say",["GET"])
+def say(text="Bonjour"):
+	try:
+		url = "http://translate.google.com/translate_tts?tl=fr&q={0}".format(urllib.quote(text[0]))
+		current_playing.append(subprocess.Popen("{0} '{1}' vlc://quit".format(args.player, url), shell=True))
+		return "{'say':true}"
+	except:
+		return "{'say':false}"
 
 
-if __name__ == '__main__':
-	serve(ip="0.0.0.0", port=5000)
+parser = argparse.ArgumentParser(description='Simple Nabz.')
+parser.add_argument('--player', action='store',help='Path to VLC command line (/usr/bin/cvlc)')
+args = parser.parse_args()
+
+
+serve(ip="0.0.0.0", port=5000)
