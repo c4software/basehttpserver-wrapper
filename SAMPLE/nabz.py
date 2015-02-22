48c91cc002215ec4a36e693ce71494bbde543a3c
Valentin Brosseau <c4software@gmail.com>
2015-02-22 22:21:00 +0100

    Add a small interface to use the Small-Nabz Script

--- SAMPLE/nabz.py
+++ SAMPLE/nabz.py
@@ -8,6 +8,73 @@ from extended_BaseHTTPServer import serve,route
 
 current_playing = []
 
+@route("/")
+def index():
+	return """
+	<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+	<form style="text-align:center" id="form">
+		<h1>Small-Nabz</h1>
+		<input type="text" id="value" value="" placeholder="http://www..." />
+		<br /><br />
+		<input type="button" value="Jouer" id="play" />
+		<input type="button" value="Parler" id="say" />
+		<input type="button" value="Stop" id="stop" />
+	</form>
+	<style>
+		body {
+			background: #E0E0E0;
+			font-family: 'Open Sans',"Trebuchet MS",arial,sans-serif;
+		}
+		textarea, input:not([type=button]):not([type=submit]) {
+			width: 80%;
+			border: 1px solid rgba(0,0,0,.2);
+			padding: .5em 1em;
+			font-family: sans-serif;
+		}
+		input[type=button], input[type=submit] {
+		  display: inline-block;
+		  position: relative;
+		  margin: 0px 20px;
+		  padding: 6px 30px;
+		  background: #333;
+		  color: #fff;
+		  box-shadow: 0 4px #111;
+		  border: none;
+		  cursor: pointer;
+		  text-transform: uppercase;
+		  letter-spacing: 2px;
+		  outline: none;
+		  -webkit-transition: none;
+		  transition: none;
+		}
+	</style>
+	<script>
+		$("#form").bind("submit", function(e){
+			e.preventDefault();
+			$("#play").click();
+		});
+		$("#play").bind('click', function(){
+			$.ajax({
+				  url: "/play",
+				  data: {file: $("#value").val()}
+				});
+		});
+
+		$("#say").bind('click', function(){
+			$.ajax({
+				  url: "/say",
+				  data: {text: $("#value").val()}
+				});
+		});
+
+		$("#stop").bind('click', function(){
+			$.ajax({
+				  url: "/stop"
+				});
+		});
+	</script>
+	"""
+
 @route("/play",["GET"])
 def play(file=""):
 	if current_playing:
@@ -37,7 +104,7 @@ def say(text="Bonjour"):
 
 
 parser = argparse.ArgumentParser(description='Simple Nabz.')
-parser.add_argument('--player', action='store',help='Path to VLC command line (/usr/bin/cvlc --play-and-exit)')
+parser.add_argument('--player', action='store',help='Path to VLC command line (/usr/bin/cvlc --play-and-exit)', default="/usr/bin/cvlc --play-and-exit")
 args = parser.parse_args()
 args.player = shlex.split(args.player)
 
